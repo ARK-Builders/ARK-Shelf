235b057081b7bfc5b344a10b8594c4c615661472
mdrlzy <mordeniusss@gmail.com>
2022-03-19 04:19:34 +0500

    use temp files

--- app/src/main/java/space/taran/arkshelf/presentation/App.kt
+++ app/src/main/java/space/taran/arkshelf/presentation/App.kt
@@ -1,9 +1,16 @@
 package space.taran.arkshelf.presentation
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import space.taran.arkshelf.di.KOIN_MODULES
+import java.nio.file.Files
+import kotlin.io.path.Path
 
 class App : Application() {
 
@@ -20,6 +27,16 @@ class App : Application() {
         startKoin {
             androidContext(this@App)
             modules(KOIN_MODULES)
+        }
+
+        cleanup()
+    }
+
+    private fun cleanup() {
+        CoroutineScope(Dispatchers.IO).launch {
+            cacheDir.listFiles()?.forEach {
+                it.deleteRecursively()
+            }
         }
     }
 }
