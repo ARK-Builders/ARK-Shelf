e12e93d6ec16a1bec37bab28c414e2c9ef4ffdce
Parvin Pathan <pgpathan22@gmail.com>
2022-07-21 17:29:09 +0530

    Added file picker lib and updated code according to this.

--- app/src/main/java/space/taran/arkshelf/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/space/taran/arkshelf/presentation/settings/SettingsFragment.kt
@@ -3,7 +3,6 @@ package space.taran.arkshelf.presentation.settings
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
 import kotlinx.coroutines.flow.collect
@@ -11,7 +10,10 @@ import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import space.taran.arkshelf.R
 import space.taran.arkshelf.databinding.FragmentSettingsBinding
-import space.taran.arkshelf.presentation.folderpicker.FolderPickerDialogFragment
+import space.taran.arkfilepicker.ArkFilePickerConfig
+import space.taran.arkfilepicker.ArkFilePickerFragment
+import space.taran.arkfilepicker.ArkFilePickerMode
+import space.taran.arkfilepicker.onArkPathPicked
 
 class SettingsFragment: Fragment(R.layout.fragment_settings) {
     private val binding by viewBinding(FragmentSettingsBinding::bind)
@@ -26,11 +28,13 @@ class SettingsFragment: Fragment(R.layout.fragment_settings) {
 
     private fun initUI() = with(binding) {
         btnChangePath.setOnClickListener {
-            val folderPicker = FolderPickerDialogFragment()
-            folderPicker.show(
-                parentFragmentManager,
-                FolderPickerDialogFragment.TAG
+            val config = ArkFilePickerConfig(
+                mode = ArkFilePickerMode.FOLDER,
+                titleStringId = R.string.app_name,
             )
+            ArkFilePickerFragment
+                .newInstance(config)
+                .show(parentFragmentManager, null)
         }
         ivBack.setOnClickListener {
             requireActivity().onBackPressed()
@@ -38,10 +42,8 @@ class SettingsFragment: Fragment(R.layout.fragment_settings) {
     }
 
     private fun initResultListener() {
-        setFragmentResultListener(
-            FolderPickerDialogFragment.REQUEST_KEY_PATH_PICKED
-        ) { _, _ ->
-            viewModel.onLinkFolderPathChanged()
+        parentFragmentManager.onArkPathPicked(lifecycleOwner = this) { path->
+            viewModel.onLinkFolderPathChanged(path)
         }
     }
 
